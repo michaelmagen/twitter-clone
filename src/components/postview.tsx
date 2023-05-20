b34f465639df614675beedd2fd42a9eda20c9095
Michael Magen <michael.magen101@gmail.com>
2023-05-19 17:42:42 -0700

    fix post date format

--- src/components/postview.tsx
+++ src/components/postview.tsx
@@ -3,10 +3,43 @@ import type { RouterOutputs } from "~/utils/api";
 import dayjs from "dayjs";
 import Image from "next/image";
 import Link from "next/link";
-
-//import relativeTime from "dayjs/plugin/relativeTime";
 import relativeTime from "dayjs/plugin/relativeTime";
+import updateLocale from "dayjs/plugin/updateLocale";
+
 dayjs.extend(relativeTime);
+dayjs.extend(updateLocale);
+
+// format relative time strings
+dayjs.updateLocale("en", {
+  relativeTime: {
+    past: "%s",
+    s: "1s",
+    m: "1s",
+    mm: "%dm",
+    h: "1h",
+    hh: "%dh",
+    d: "1 day",
+    dd: "%d days",
+  },
+});
+
+// takes a date and returns a formated string
+function dateToFormatedString(datePrimitive: Date) {
+  const date = dayjs(datePrimitive);
+
+  // if post created less than a day ago, use relative time
+  const isLessThan24HoursAgo = date.isAfter(dayjs().subtract(24, "hour"));
+  if (isLessThan24HoursAgo) {
+    return date.fromNow();
+  }
+
+  // use normal date for other dates
+  // date string only contains year if date not in current year
+  const isCurrentYear = date.isSame(new Date(), "year");
+  const formatString = isCurrentYear ? "MMM DD" : "MMM DD, YYYY";
+
+  return date.format(formatString);
+}
 
 type PostWithUser = RouterOutputs["posts"]["getAllInfinite"][number];
 export const PostView = (props: PostWithUser) => {
@@ -28,9 +61,12 @@ export const PostView = (props: PostWithUser) => {
             <span className="font-thin text-gray-400">{`  @${author.username}`}</span>
           </Link>
           <Link href={`/post/${post.id}`}>
-            <span className="font-thin text-gray-400">{` · ${dayjs(
+            {/* <span className="font-thin text-gray-400">{` · ${dayjs(
               post.createdAt
-            ).fromNow()}`}</span>
+            ).fromNow()}`}</span> */}
+            <span className="font-thin text-gray-400">
+              {` · ${dateToFormatedString(post.createdAt)}`}
+            </span>
           </Link>
         </div>
         <span className="text-base">{post.content}</span>
