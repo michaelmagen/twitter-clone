2731c317b824ccff612a3d82cd52b216cf35f036
Michael Magen <michael.magen101@gmail.com>
2023-06-22 12:14:14 -0700

    fix incorrect path properties

--- src/components/icons/HomeIcon.tsx
+++ src/components/icons/HomeIcon.tsx
@@ -11,8 +11,8 @@ export const HomeIcon = () => {
       <path
         d="M12 1.696L.622 8.807l1.06 1.696L3 9.679V19.5C3 20.881 4.119 22 5.5 22h13c1.381 0 2.5-1.119 2.5-2.5V9.679l1.318.824 1.06-1.696L12 1.696zM12 16.5c-1.933 0-3.5-1.567-3.5-3.5s1.567-3.5 3.5-3.5 3.5 1.567 3.5 3.5-1.567 3.5-3.5 3.5z"
         fill="currentColor"
-        fill-rule="evenodd"
-        clip-rule="evenodd"
+        fillRule="evenodd"
+        clipRule="evenodd"
       ></path>
     </svg>
   );
